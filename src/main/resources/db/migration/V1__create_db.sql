6fbe65c89f92d77e4ebd47d9c1e6615357f11a2d
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-09-14 19:14:30 +0300

    data

---
+++ src/main/resources/db/migration/V1__create_db.sql
@@ -0,0 +1,5 @@
+create TABLE client (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	title VARCHAR (50) NOT NULL CHECK (CHAR_LENGTH(title) >= 3),
+	content VARCHAR (2000) NOT NULL CHECK (CHAR_LENGTH(content) >= 3)
+);
