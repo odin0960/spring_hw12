46a3c3cde6b55d203adab83077b3e3c36835478a
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-09-21 21:27:27 +0300

    NoteService Interface

--- src/main/java/com/goit12/todolist/entity/DTO/UserDTO.java
+++ src/main/java/com/goit12/todolist/entity/DTO/UserDTO.java
@@ -1,12 +1,12 @@
 package com.goit12.todolist.entity.DTO;
 
+import com.goit12.todolist.entity.User;
 import lombok.Data;
-
 
 @Data
 public class UserDTO {
     private Long Id;
     private String username;
 //    private String password;
-    private String role;
+    private User.Role role;
 }
